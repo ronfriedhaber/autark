4f6e36881c48eb40754c3d54b270702cda052995
Ron Friedhaber <ronfriedhaber@users.noreply.github.com>
2026-02-04 14:36:23 +0200

    :: rem half-correct Display impl for OpPool.

--- crates/mpera/src/op/oppool.rs
+++ crates/mpera/src/op/oppool.rs
@@ -1,5 +1,3 @@
-use std::fmt::{self};
-
 use crate::op::{Op, OpRef};
 
 #[derive(Debug, Clone)]
@@ -28,16 +26,5 @@ impl<'a> IntoIterator for &'a OpPool {
 
     fn into_iter(self) -> Self::IntoIter {
         self.0.iter()
-    }
-}
-
-impl fmt::Display for OpPool {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for (ix, _x) in self.0.iter().enumerate() {
-            write!(f, "OP{} ", ix)?;
-            // x.fmt(f);
-            write!(f, "\n")?;
-        }
-        Ok(())
     }
 }
