b7849c3a7334d5d6ec424d71effe60113332e675
Ron Friedhaber <ronfriedhaber@users.noreply.github.com>
2026-02-05 11:47:34 +0200

    :: test extend

--- crates/autark-client/tests/rolling_a.rs
+++ crates/autark-client/tests/rolling_a.rs
@@ -15,7 +15,14 @@ fn hash_of_t<T: Hash>(x: &T) -> Option<u64> {
 
 #[test]
 fn t1() -> Result<()> {
-    let input = record_batch!(("a", Int32, [1, 2, 3]))?;
+    let input = record_batch!(
+        ("a", Int32, [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]),
+        (
+            "b",
+            Float32,
+            [0.1, 0.2, 0.4, 0.8, 1.6, 3.2, 6.4, 12.8, 25.6, 51.2, 102.4]
+        )
+    )?;
     let reader = ArrowReader::new(input);
 
     let frame = OnceFrame::new(reader, SinkVoid {});
@@ -25,13 +32,28 @@ fn t1() -> Result<()> {
         .dataframe(None)?
         .col("a")?
         .reduce(ReduceOpKind::Sum)?
-        .alias("a_sum", None)?; // Shall automatixally detect result is 0-d scalar and not return a frame.
+        .alias("a_sum", None)?; // Shall automatically detect result is 0-d scalar and not return a frame.
+
+    frame
+        .p
+        .dataframe(None)?
+        .col("b")?
+        .reduce(ReduceOpKind::Sum)?
+        .alias("b_sum", None)?; // Shall automatically detect result is 0-d scalar and not return a frame.
+
+    frame
+        .p
+        .dataframe(None)?
+        .col("a")?
+        .rolling(4)?
+        .reduce(ReduceOpKind::Mean)?
+        .alias("rolling_mean", None)?;
 
     let realized = frame.realize()?;
     dbg!(hash_of_t(&realized));
 
     // IMPORTANT: If underlying transformation is mutated, the Hash ought to be recomputed.
-    assert_eq!(hash_of_t(&realized), Some(9527474090956641498));
+    assert_eq!(hash_of_t(&realized), Some(4566204555854787833));
 
     Ok(())
 }
