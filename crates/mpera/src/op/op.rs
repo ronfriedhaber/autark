b217d674cd6ea1e42423bc8f885feb291eb18e80
Ron Friedhaber <ronfriedhaber@users.noreply.github.com>
2026-02-05 12:22:39 +0200

    :: chore — rem dead code

--- crates/mpera/src/op/op.rs
+++ crates/mpera/src/op/op.rs
@@ -71,27 +71,3 @@ pub enum Op {
         ascending: bool,
     },
 }
-
-// im
-// pl fmt::Display for Op {
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         match self {
-//             Op::BinaryOp { kind, lhs, rhs } => {
-//                 write!(f, "BIN {:?} {}, {}", kind, lhs.0, rhs.0)
-//             }
-//             Op::Reduce { kind, on } => {
-//                 write!(f, "REDUCE {:?} {}", kind, on.0)
-//             }
-//             Op::Filter { on, mask } => {
-//                 write!(f, "FILTER ON {:?} MASK{:?}", on, mask)
-//             }
-//             Op::Column { dataframe, column } => {
-//                 if let Some(df) = dataframe {
-//                     write!(f, "COLUMN df{}.{}", df, column)
-//                 } else {
-//                     write!(f, "COLUMN {}", column)
-//                 }
-//             }
-//         }
-//     }
-// }
